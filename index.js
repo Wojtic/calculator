f0921aeb5550fd86114f6d1f711d7c47d835bc71
Wojtic <vojtacerny012@gmail.com>
2022-05-20 18:12:30 +0200

    Basic structure for calculator

--- index.js
+++ index.js
@@ -12,6 +12,44 @@ abc_tlacitko.onclick = setup_abc;
 setup_abc();
 setupVstupyEventListenery();
 // -------------------------------------------------------------------- kalkulačka obecné
-let soustava = 2;
+const soustava_vyber = document.querySelector(".inpt_soustava");
+const mode42_toggle = document.querySelector(".check_42");
 
+let SOUSTAVA = soustava_vyber.value;
+let MODE42 = mode42_toggle.value;
+let kalkulacka = "nothing";
+
+soustava_vyber.onchange = () => (SOUSTAVA = soustava_vyber.value);
+mode42_toggle.onchange = () => (MODE42 = mode42_toggle.value);
+const kalkulacka_obal = document.querySelector(".calculator_inside");
+const kalkulacka_vyber = document.querySelector(".calc_mode_select");
+kalkulacka_vyber.onchange = () => {
+  kalkulacka = kalkulacka_vyber.value;
+  renderCalc();
+};
+
+function renderCalc() {
+  switch (kalkulacka) {
+    case "nothing":
+      kalkulacka_obal.innerHTML = "";
+      break;
+    case "random":
+      renderRandomCalc();
+      kalkulacka_obal.innerHTML = "náhodné";
+      break;
+    case "normal":
+      renderNormalCalc();
+      kalkulacka_obal.innerHTML = "normální";
+      break;
+    default:
+      kalkulacka_obal.innerHTML = "";
+      break;
+  }
+}
 // --------------------------------------------------------------------- náhodná kalkulačka
+
+function renderRandomCalc() {}
+
+// --------------------------------------------------------------------- normální kalkulačka
+
+function renderNormalCalc() {}
